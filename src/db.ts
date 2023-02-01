1fec4f0463a76404d690296afb2594bbe9663248
Kedap <kedap.dev@protonmail.com>
2023-01-31 21:57:38 -0600

    feat: se puede realizar registros con sqlite

---
+++ src/db.ts
@@ -0,0 +1,106 @@
+import { Database } from 'sqlite';
+import { ensureFile } from 'std/fs/mod.ts';
+
+export interface Configuracion {
+  id: number;
+  nombre: string;
+  modelo: string;
+  temperatura: number;
+}
+
+export interface Respuesta {
+  id: number;
+  nombre: string;
+  hora: string;
+  respuesta: string;
+  entrada: string;
+}
+
+export function obtener_conn(archivo: string) {
+  return new Database(archivo);
+}
+
+function query(q: string, db: Database) {
+  db.exec(q);
+}
+
+function existe_schema() {
+  const db = new Database('usuarios.db');
+  const tablas = db
+    .prepare(
+      `SELECT name FROM sqlite_schema
+WHERE type='table'
+ORDER BY name`
+    )
+    .all();
+
+  try {
+    return tablas[0].name == 'config' && tablas[1].name == 'respuestas';
+  } catch {
+    return false;
+  }
+  // return tablas[0].name == 'config' && tablas[0].name == 'config';
+}
+
+export class Usuarios {
+  db: Database;
+  constructor(
+    db_path?: string,
+    configuracion?: Configuracion,
+    entrada?: Respuesta
+  ) {
+    const config = `
+     CREATE TABLE config (
+     id INTEGER PRIMARY KEY,
+     nombre TEXT NOT NULL,
+     modelo TEXT NOT NULL,
+     temperatura REAL NOT NULL
+    );`;
+    const respuestas = `
+     CREATE TABLE respuestas (
+     id INTEGER PRIMARY KEY AUTOINCREMENT,
+     id_chat INTEGER,
+     nombre TEXT NOT NULL,
+     hora TEXT NOT NULL,
+     respuesta TEXT NOT NULL,
+     entrada TEXT NOT NULL
+    );`;
+
+    if (!db_path) {
+      db_path = 'usuarios.db';
+    }
+    ensureFile(db_path);
+    const db = new Database(db_path);
+    this.db = db;
+    if (existe_schema()) {
+      return this;
+    }
+    query(config, db);
+    query(respuestas, db);
+
+    if (configuracion) {
+      this.agregar_conf(configuracion);
+    }
+    if (entrada) {
+      this.agregar_resp(entrada);
+    }
+
+    return this;
+  }
+
+  agregar_conf(c: Configuracion) {
+    const consulta = `
+      INSERT INTO config (id, nombre, modelo, temperatura)
+      VALUES ('${c.id}', '${c.nombre}', '${c.modelo}', '${c.temperatura}')
+    `;
+    query(consulta, this.db);
+  }
+
+  agregar_resp(r: Respuesta) {
+    const consulta = `
+      INSERT INTO respuestas (id_chat, nombre, hora, respuesta, entrada)
+      VALUES ('${r.id}', '${r.nombre}', '${r.hora}', '${r.respuesta}', '${r.entrada}')
+    `;
+    query(consulta, this.db);
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,40 @@
-import { Bot } from "grammy/mod.ts";
-import { config } from "dotenv";
+import { Bot } from 'grammy/mod.ts';
+import { config } from 'dotenv';
+import { Usuarios, Respuesta } from './db.ts';
 
+// Configuracion de los tokens
 const bot = new Bot(config().BOT_TOKEN);
-bot.command("start", (ctx) => ctx.reply("Hola mundo!"));
-bot.on("message:text", (ctx) => {
-  ctx.reply("No es una fototet4 :C");
-  ctx.reply("No entiendo: " + ctx.update.message.text);
+const db = new Usuarios();
+const ahora = new Date();
+
+bot.command('start', (ctx) => ctx.reply('Hola mundo!'));
+bot.on('message:text', (ctx) => {
+  // const hora = `${ahora.getDay}/${ahora.getMonth}/${ahora.getFullYear}-${ahora.getHours}:${ahora.getMinutes}:${ahora.getSeconds}`;
+  const hora: string =
+    ahora.getDay() +
+    '/' +
+    ahora.getDate().toString() +
+    '/' +
+    ahora.getMonth() +
+    '/' +
+    ahora.getFullYear().toString() +
+    ':' +
+    ahora.getHours().toString() +
+    ':' +
+    ahora.getMinutes().toString() +
+    ':' +
+    ahora.getSeconds().toString();
+
+  const autor = ctx.update.message.from;
+  const respuesta: Respuesta = {
+    id: autor.id,
+    nombre: autor.first_name,
+    hora: hora,
+    respuesta: '',
+    entrada: ctx.update.message.text,
+  };
+  ctx.reply('No entiendo: ' + ctx.update.message.text);
+  db.agregar_resp(respuesta);
 });
 
 bot.start();
